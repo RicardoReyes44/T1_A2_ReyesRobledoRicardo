b53126a97574aa2d62e506bbccf9e764abad1d54
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-05 14:48:22 -0600

    Se extendio la clase 'Interfaz' para que herede de la clase 'JFrame'

--- src/PruebaInterfaz.java
+++ src/PruebaInterfaz.java
@@ -6,12 +6,33 @@ import javax.swing.*;
 import javax.swing.text.MaskFormatter;
 
 
+class Interfaz extends JFrame{
+	
+	public Interfaz() {
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800, 630);
+    	setTitle("Primera tarea");
+    	setLocationRelativeTo(null);
+    	
+    	setVisible(true);
+	}
+	
+}
+
 
 public class PruebaInterfaz extends JFrame{
 
 	public static void main(String[] args) {
 		
-        
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Interfaz();
+				}
+		});
 
 	}
 
