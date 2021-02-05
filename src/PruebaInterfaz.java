b3fa62bd00bfc9d8b37a427e7ba6d4aa39614af3
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-05 13:38:09 -0600

    Se creo el proyecto y se agrego la clase de prueba

---
+++ src/PruebaInterfaz.java
@@ -0,0 +1,18 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.text.ParseException;
+import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+
+
+
+public class PruebaInterfaz extends JFrame{
+
+	public static void main(String[] args) {
+		
+        
+
+	}
+
+}
